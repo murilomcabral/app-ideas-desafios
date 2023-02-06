79a03f1aa7e273ba72eaf92f974a07dd5fbce4dc
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-02-06 00:43:18 -0300

    trying hard

--- beginner/calculator/script.js
+++ beginner/calculator/script.js
@@ -1,36 +1,66 @@
-const currentNum = document.querySelector('.current-num');
-const previousNum = document.querySelector('.previous-num');
-const numbers = document.querySelectorAll('.numero');
+const currentNumOutput = document.querySelector('.current-num');
+const previousNumOutput = document.querySelector('.previous-num');
 const clearBtt = document.querySelector('.clear');
 const allClearBtt = document.querySelector('.all-clear');
+const numbers = document.querySelectorAll('.numero');
+const operator = document.querySelectorAll('.operador');
+const delBtt = document.querySelector('.del');
+let calc = 0
 
 class Calculadora {
-  currentNum
-  previousNum
+  currentNumOutput
+  previousNumOutput
 
-  constructor(currentNum, previousNum){
-    this.currentNum = currentNum;
-    this.previousNum = previousNum;
+  constructor(currentNumOutput, previousNumOutput){
+    this.currentNumOutput = currentNumOutput;
+    this.previousNumOutput = previousNumOutput;
   }
 
   appendNum(dig) {
-    if(dig === ',' && currentNum.textContent.includes(',')) {return}
-    if(dig === '0' && currentNum.textContent === '0') {return}
-    if(dig !== '0' && dig !== ',' && currentNum.textContent === '0') {currentNum.textContent = ''}
-    currentNum.textContent += dig;
+    if(dig === '.' && currentNumOutput.textContent.includes('.')) {return}
+    if(dig === '0' && currentNumOutput.textContent === '0') {return}
+    if(dig !== '0' && dig !== '.' && currentNumOutput.textContent === '0') {currentNumOutput.textContent = ''}
+    currentNumOutput.textContent += dig;
   }
 
   clear() {
-    currentNum.textContent = '0';
+    currentNumOutput.textContent = '0';
   }
 
   allClear() {
-    previousNum.textContent = '0';
-    currentNum.textContent = '0';
+    previousNumOutput.textContent = '0';
+    currentNumOutput.textContent = '0';
   }
+
+  del() {
+    if (currentNumOutput.textContent.length === 1) {return currentNumOutput.textContent = 0}
+    currentNumOutput.textContent = currentNumOutput.textContent.slice(0, currentNumOutput.textContent.length -1)
+  }
+
+  calculate(elem) {
+    let pNum = Number(previousNumOutput.textContent)
+    let cNum = Number(currentNumOutput.textContent)
+    switch (elem) {
+      case '÷':
+        calc = pNum / cNum      
+        break
+      case 'x':
+        calc = pNum * cNum    
+        break
+      case '-':
+        calc = pNum - cNum    
+        break
+      case '+':
+        calc = pNum + cNum    
+        break
+    }
+    previousNumOutput.textContent = calc;
+    currentNumOutput.textContent = 0
+  }
+  
 }
 
-const calculadora = new Calculadora(currentNum, previousNum)
+const calculadora = new Calculadora(currentNumOutput, previousNumOutput)
 
 numbers.forEach((elem) => {
   elem.addEventListener('click', (event) => {
@@ -38,10 +68,12 @@ numbers.forEach((elem) => {
   })
 })
 
-function calcPad(event) {
-  console.log(event.key)
-}
+operator.forEach((elem) => {
+  elem.addEventListener('click', (event) => {
+    calculadora.calculate(event.target.textContent)
+  })
+})
 
-clearBtt.addEventListener('click', calculadora.clear)
 allClearBtt.addEventListener('click', calculadora.allClear)
-window.addEventListener('keydown', calcPad)
+clearBtt.addEventListener('click', calculadora.clear)
+delBtt.addEventListener('click', calculadora.del)
